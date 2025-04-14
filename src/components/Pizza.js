45f141a3314511e817877774a0026e291d821626
NurkaAmre <nurkerey30@gmail.com>
2025-04-14 20:50:56 +0200

    Implement Pizza component

--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
-export default function Pizza() {
-  return <div></div>;
+export default function Pizza({ name, ingredients, photoName, price }) {
+  return (
+    <li className="pizza">
+      <img src={photoName} />
+      <div>
+        <h3>{name}</h3>
+        <p>{ingredients}</p>
+        <span>{price}</span>
+      </div>
+    </li>
+  );
 }
