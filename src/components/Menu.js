7a82597f5f5918f61f3252c16e2ff7b662eb6e14
NurkaAmre <nurkerey30@gmail.com>
2025-04-14 20:43:40 +0200

    Implemented menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import '../index.css';
 
 export default function Menu() {
-  return <main></main>;
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+      <>
+        <p>
+          Authentic Italian cuisine. 6 creative dishes to choose from. All from
+          our stone oven, all organic, all delicious.
+        </p>
+        <ul className="pizzas"></ul>
+      </>
+    </main>
+  );
 }
