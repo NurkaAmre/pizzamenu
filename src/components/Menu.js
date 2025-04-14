50d0b47c78f228636041c8b64ea056e5092dfebf
NurkaAmre <nurkerey30@gmail.com>
2025-04-14 20:55:37 +0200

    Render our pizzas inside menu component

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../index.css';
+import Pizza from './Pizza';
 
 export default function Menu() {
   return (
@@ -10,7 +11,15 @@ export default function Menu() {
           Authentic Italian cuisine. 6 creative dishes to choose from. All from
           our stone oven, all organic, all delicious.
         </p>
-        <ul className="pizzas"></ul>
+
+        <ul className="pizzas">
+          <Pizza
+            name="Pizza Spinacci"
+            ingredients="Mozarella, tomato, spinach and ricotta cheese"
+            photoName="./public.spinaci.jpg"
+            price={12.99}
+          />
+        </ul>
       </>
     </main>
   );
